2fa517457bb06eed1fce34627dc353d58679aa59
Steve Tentrop <steve.tentrop@plentymarkets.com>
2019-02-07 13:15:35 +0100

    fix .toFixed function

--- resources/js/src/app/filters/currency.filter.js
+++ resources/js/src/app/filters/currency.filter.js
@@ -8,6 +8,21 @@ Vue.filter("currency", function(price)
     {
         return price;
     }
+    var number = price;
 
-    return formatter.format(parseFloat(price).toFixed(2), App.activeCurrency);
+    if (typeof price !== "undefined")
+    {
+        var splittedPrice = price.toString().split(".");
+
+        if (!splittedPrice[1])
+        {
+            number = splittedPrice[0];
+        }
+        else if (splittedPrice[1].length > 2)
+        {
+            number = splittedPrice.join(".") + "1";
+        }
+    }
+
+    return formatter.format(parseFloat(number).toFixed(2), App.activeCurrency);
 });
